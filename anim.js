0bbe8db812aff65084f9983d7b23c3449b7f36b9
tetralovania <tetralovania@gmail.com>
2023-05-17 19:13:30 +0200

    added background, made header better, added some more anims

--- anim.js
+++ anim.js
@@ -1,2 +1,6 @@
-gsap.from(".header", {y: -100, duration: .5, ease: "expo.out"});
-gsap.from(".menu-item", {y: -100, duration: .3, ease: "expo.out", stagger:.1, delay: .1});
+let tl = new gsap.timeline();
+
+tl.from(".overlay", {backgroundColor: "#241c006e", duration: .5, ease:"none"});
+tl.from(".header", {y: -100, duration: .5, ease: "expo.out"});
+tl.from(".title", {y: -100, duration: .3, ease: "expo.out", stagger:.05, delay: -.4});
+tl.from(".menu-item", {y: -100, duration: .3, ease: "expo.out", stagger:.05, delay: -.3});
---
+++ responsiveheader.js
@@ -0,0 +1,11 @@
+// var elemWidth, fitCount, fixedWidth = 120,  
+//     $menu = $(".header"), $collectedSet;
+// collect();
+// $(window).resize(collect);
+
+// function collect() {
+//     elemWidth = $menu.width();
+//     fitCount = Math.floor(elemWidth / fixedWidth) - 1;
+//     $collectedSet = $menu.children(":gt(" + fitCount + ")");
+//     $("#submenu").empty().append($collectedSet.clone());    
+// }
